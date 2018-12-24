8c74f27ea1a6db229b1705ae6282798944657114
ghost0fharambe <43300063+ghost0fharambe@users.noreply.github.com>
2018-12-23 20:59:38 -0700

    UPDATE query working, basic functionality established

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -49,6 +49,7 @@ function readProducts() {
 };
 
 function buyItem() {
+    console.log("Item Purchase Interface......\n")
     inquirer
         .prompt([{
                 name: "id",
@@ -72,19 +73,29 @@ function buyItem() {
                     return false;
                 }
             }
-        ]).then(function(answer) {
-            console.log(answer);
+        ]).then(function (answer) {
+            // console.log(answer + "\n");
             // var query = "SELECT item_ID, product_name, price FROM products WHERE item_ID = 1"
-            connection.query("SELECT item_ID, product_name, price FROM products WHERE item_ID = ?", [answer.id], function(err, response){
-                console.log(response[0].item_ID + ' | ' + response[0].product_name + ' | ' + '$' + response[0].price);
+            connection.query("SELECT item_ID, product_name, price, stock_quantity FROM products WHERE item_ID = ?", [answer.id], function (err, response) {
+                console.log("\n ID: " + response[0].item_ID + ' | ' + "Product: " + response[0].product_name + ' | ' + 'Price: $' + response[0].price + '\n');
                 //console.log(query.sql)
+                if (response[0].stock_quantity < answer.amount) {
+                    console.log("Sorry, we do not have enough inventory to meet your request.")
+                } else {
+                    console.log('Hoopla')
+                    var unitAmount = parseInt(response[0].stock_quantity - answer.amount);
+                    var totalCost = parseInt(response[0].price * answer.amount);
+                    //console.log(unitAmount);
+                    updateStock(unitAmount, response[0].product_name, totalCost, answer);
+                }
                 connection.end();
             })
         })
 }
 
-// var query = connection.query("SELECT item_ID,product_name,price FROM products WHERE ?", { item_ID: answer.product }, function (err, res) {
-//     //if (err) throw err;
-//     console.log(res);
-//     console.log(query.sql);
-// })
+function updateStock(unitAmount, resp, totalCost, answer) {
+    connection.query('UPDATE products SET stock_quantity = ? WHERE item_ID = ?', [unitAmount, answer.id], function (err, response) {
+        console.log(`You have purchased ${answer.amount} unit(s) of ${resp}\n`);
+        console.log(`Total Cost: ${totalCost}`);
+    })
+};
